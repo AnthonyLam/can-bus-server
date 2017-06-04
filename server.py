e96fc712e0cc862a0bd9b1c85d3506918a8b515f
Anthony Lam <thelamacmdr@gmail.com>
2017-06-04 14:20:48 -0700

    Changes

---
+++ server.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+
+class CanHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST():
+        pass
+
+    def do_GET():
+        pass
+
+class AiHttpRequestHandler(BaseHTTPRequestHandler):
+    pass
