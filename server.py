82d53cb338d455370f16034d55f3dd8551bebcc1
Anthony Lam <thelamacmdr@gmail.com>
2017-06-04 17:13:28 -0700

    Data

--- server.py
+++ server.py
@@ -1,12 +1,29 @@
 from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
 
 class CanHTTPRequestHandler(BaseHTTPRequestHandler):
 
-    def do_POST():
-        pass
+
+    def _response(self, data):
+        self.send_response(200)
+        self.send_header("Content-Length", len(data))
+        self.end_headers()
+        self.wfile.write(data)
+
+    def do_POST(self):
+        self._response("Done")
+        print(str(self.rfile.read()))
+
 
     def do_GET():
         pass
 
 class AiHttpRequestHandler(BaseHTTPRequestHandler):
     pass
+
+def run(server_class=HTTPServer, server_handler=CanHTTPRequestHandler):
+    server = server_class(('',8080),server_handler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    run()
